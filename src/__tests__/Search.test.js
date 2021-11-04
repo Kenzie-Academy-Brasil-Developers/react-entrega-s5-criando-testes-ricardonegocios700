abeab557480adfb977069e976446a09b7745a9a9
Ricardo <ricardonegocios700@gmail.com>
2021-11-04 17:52:16 -0300

    finish

--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -1,15 +1,15 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Search from "../components/Search";
+import Cep from "../components/Cep";
 import Providers from "../providers";
 import api from "../services/index";
 import MockAdapter from "axios-mock-adapter";
-import userEvent from "@testing-library/user-event";
 
 describe("texto", () => {
   it("texto", async () => {
     //preparo o ambiente com dados
     const apiMock = new MockAdapter(api);
-    apiMock.onPost("/").replyOnce(200, {
+    apiMock.onGet("01311100").replyOnce(200, {
       bairro: "Bela Vista",
       cep: "01311100",
       cidade: "São Paulo",
@@ -27,17 +27,24 @@ describe("texto", () => {
     render(
       <Providers>
         <Search />
+        <Cep />
       </Providers>
     );
 
     const cepField = screen.getByPlaceholderText("Insira o CEP");
-    const buttonElement = screen.getByText("Buscar pelo CEP");
+    //const buttonElement = screen.getByText("Buscar pelo CEP");
+    const buttonElement = screen.getByRole("button");
 
     fireEvent.change(cepField, { target: { value: "01311100" } });
     fireEvent.click(buttonElement);
 
-    const result = await screen.findByDisplayValue("Avenida Paulista");
+    await waitFor(() => {
+      const result = screen.getByText("Logradouro");
+      expect(result).toBeInTheDocument();
+    });
 
-    expect(result).toBeInTheDocument();
+    /* outra tentativa foi
+    const result = await screen.findByDisplayValue("Avenida Paulista");
+    expect(result).toBeInTheDocument();*/
   });
 });
